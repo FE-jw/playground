6f3ef7265b8d0c32f362a9762c632d3be9d135fc
JW <zaixu91@gmail.com>
2024-04-18 22:01:35 +0900

    vue 모듈 테스트

---
+++ vue-cli-module/src/App.vue
@@ -0,0 +1,13 @@
+<template>
+  <button type="button" @click="evtHandler">버튼</button>
+</template>
+
+<script setup>
+const evtHandler = () => {
+  console.log('evtHandler');
+};
+</script>
+
+<style scoped>
+button  {background-color:#000;color:#fff;}
+</style>
