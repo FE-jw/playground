090c9aae03a1471aa9ff602864b90c9a019f3b0e
jw <zaixu91@gmail.com>
2024-11-01 18:30:27 +0900

    테스트

---
+++ next-test/src/app/page.js
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis earum vel aspernatur dicta natus dolorum, minus maxime culpa fuga veritatis nostrum repellat quasi et explicabo necessitatibus eos temporibus tenetur incidunt illo dolore! Numquam quisquam sed repudiandae consectetur doloremque voluptatem ipsum rerum quibusdam eos fugiat velit reiciendis blanditiis, eius, vel enim natus provident illo consequatur iure. Omnis architecto animi eaque aspernatur quia laudantium est assumenda ratione. Magnam, consequatur soluta laudantium nihil delectus tenetur rerum suscipit dolores laborum nemo provident commodi. Assumenda consequuntur dignissimos, quibusdam nesciunt dolorem possimus, repudiandae et amet nulla autem ullam reprehenderit! Consequuntur voluptas recusandae voluptatum autem porro officiis.
+    </>
+  );
+}
---
+++ react-test/src/App.js
@@ -0,0 +1,11 @@
+// import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, similique?
+    </div>
+  );
+}
+
+export default App;
