cce789469eda894ef44fa5e40d86ac3eba3d0c06
jw <zaixu91@gmail.com>
2025-02-13 17:08:50 +0900

    tailwind css 테스트
    - base, components 는 불필요할수도

--- turbo-test/apps/app-4-redux/src/app/page.tsx
+++ turbo-test/apps/app-4-redux/src/app/page.tsx
@@ -5,8 +5,10 @@ export default function Home() {
   return (
     <>
       <Btn />
-      <br></br>
-      <Link href="/intro">intro 이동</Link>
+      <br />
+      <Link className="inline-flex p-[20px]" href="/intro">
+        intro 이동
+      </Link>
       <Link href="/introaaa">404 이동</Link>
     </>
   );
