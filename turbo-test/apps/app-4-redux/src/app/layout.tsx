d89cf1768949da6aa96e4977b32eeda2ae2064a7
jw <zaixu91@gmail.com>
2025-02-13 13:54:10 +0900

    redux 예제 프로젝트 생성

--- turbo-test/apps/app-4-redux/src/app/layout.tsx
+++ turbo-test/apps/app-4-redux/src/app/layout.tsx
@@ -2,8 +2,8 @@ import type {Metadata} from 'next';
 import './globals.css';
 
 export const metadata: Metadata = {
-  title: 'app-4-redux',
-  description: 'app-4-redux',
+  title: 'app-4-redux title',
+  description: 'app-4-redux description',
 };
 
 export default function RootLayout({
