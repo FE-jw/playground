bf268fc1c8227974fabfb627b8e724a67046cd61
jw <zaixu91@gmail.com>
2025-02-18 17:51:39 +0900

    useId() 테스트, 오타 수정

--- turbo-test/apps/app-1/src/app/page.js
+++ turbo-test/apps/app-1/src/app/page.js
@@ -2,17 +2,22 @@
 
 import {Provider} from 'react-redux';
 import {store} from '@/store';
+import {useId} from 'react';
 
 // 공통 컴포넌트
 import MyBtn from '@repo/ui/mybtn';
 import MySwiper from '@repo/ui/myswiper';
 
-// 컴포펀트
+// 컴포넌트
 import BtnCount from '@/components/BtnCount';
 import MyTest from '@/components/MyTest';
 
 export default function Home() {
   const tempList = [{name: 'jw'}, {name: 'a'}, {name: 'b'}, {name: 'c'}];
+
+  for (let index = 0; index < 100; index++) {
+    console.log(`${index}: `, useId());
+  }
 
   const handleClickBtn = e => {
     console.log(e);
