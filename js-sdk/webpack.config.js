5ba56f268bf283b378fc42ee7192f31b7577b680
jw <zaixu91@gmail.com>
2026-01-14 13:43:02 +0900

    [js-sdk] dev 모드 실행 시 index.html 파일 열고, 수정 시 자동 새로고침

--- js-sdk/webpack.config.js
+++ js-sdk/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = (env, argv) => {
   const isProd = argv.mode === 'production';
@@ -15,6 +16,32 @@ module.exports = (env, argv) => {
       libraryExport: 'TestSDK'
     },
     devtool: isProd ? 'source-map' : 'eval-cheap-module-source-map',
+    devServer: {
+      static: {
+        directory: path.join(__dirname, 'dist')
+      },
+      compress: true,
+      port: 3000,
+      open: true, // 서버 실행 시 브라우저 자동 실행
+      hot: true, // Hot Module Replacement 활성화
+      watchFiles: ['src/**/*'] // src 폴더 내 모든 파일 변경 감지 (html 포함)
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: './src/index.html' // src/index.html을 템플릿으로 사용
+      })
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader'
+          }
+        }
+      ]
+    },
     /*
     module: {
       rules: [
